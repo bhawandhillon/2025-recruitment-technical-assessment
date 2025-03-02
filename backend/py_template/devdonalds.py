e1c00950b21065eb61324e6fc7a32ca2c7b8d07e
Bhawanjot Singh Dhillon <z5358995@ad.unsw.edu.au>
2025-03-02 12:11:59 +1100

    Finished task 3

--- backend/py_template/devdonalds.py
+++ backend/py_template/devdonalds.py
@@ -120,7 +120,7 @@ def create_entry():
 	
 	# All error cases have been tested. Add the new entry to the cookbook
 	cookbook.append(data)
-	
+
 	# return a HTTP 200 status code and an empty response body
 	return {}, 200
 
@@ -130,8 +130,96 @@ def create_entry():
 # Endpoint that returns a summary of a recipe that corresponds to a query name
 @app.route('/summary', methods=['GET'])
 def summary():
-	# TODO: implement me
-	return 'not implemented', 500
+
+	# Extract the recipe name from the URL
+	recipeName = request.args.get('name')
+
+	# Check if a recipe with the corresponding name exists in the cookbook.
+	# If it does not return an error
+	recipeToSummarise = None
+
+	for index in cookbook:
+		if index['name'] == recipeName and index['type'] == 'recipe':
+			recipeToSummarise = index
+			break
+	
+	if recipeToSummarise is None:
+		return jsonify({"Error": "A recipe with the corresponding name cannot be found."}), 400
+	
+	# Try a helper function which return a List of the base ingredients
+	# and their quantity. It will raise an exception if the recipe contains
+	# recipes or ingredients that aren't in the cookbook
+	try:
+		baseIngredientsList = baseIngredientsHelper(recipeToSummarise)
+	except Exception as e:
+		return jsonify({"Error": str(e)}), 400
+	
+	# Call a helper function with the baseIngredients to get the total
+	# cooking time
+	cookTime = cookTimeHelper(baseIngredientsList)
+
+	# Create the summary object
+	recipeSummary = {
+		"name": recipeName, 
+		"cookTime": cookTime,
+		"ingredients": baseIngredientsList
+	}
+
+
+	return recipeSummary, 200
+
+# This function takes in a recipe and returns a list containing the base
+# ingredients and their quantity
+def baseIngredientsHelper(recipe, factor=1):
+	
+	# The dictionary of base ingredients and their quantities to return
+	baseIngredientsReturnDict = {}
+
+	# For each required ingredient or recipe check that it is in the cookbook.
+	# If it is the cookbook save it as we will need to explore it to determine
+	# if it is a base ingredient or not and recurive in the latter case.
+	# If the reqiured ingredient does not exist in the cookbook then
+	# raise an exception
+	for requiredItem in recipe["requiredItems"]:
+		itemToExplore = None
+		for existentItem in cookbook:
+			if requiredItem["name"] == existentItem["name"]:
+				itemToExplore = existentItem
+				break
+	
+	if itemToExplore is None:
+		raise Exception("The recipe contains recipes or ingredients that aren't in the cookbook. Please fix this and try again.")
+	
+	# Since the item does exist in the cookbook multiply the factor to account
+	# for nested recipes
+	quantity = requiredItem["quantity"] * factor
+
+	# If the itemToExplore is a recipe then recurse to get the base ingredients
+	# and also set the factor to the current quantity (to account for the
+	# nesting of recipes). Then add those base ingredients to the 
+	# baseIngredientsReturnDict.
+	# If the itemToExplore is an ingredient and it exists in the 
+	# baseIngredientsReturnDict then add the quantity to the
+	# existing quantity otherwise add a new key value pair for the 
+	# itemToExplore and its quantity.
+	if itemToExplore["type"] == "recipe":
+		baseIngredients = baseIngredientsHelper(itemToExplore, factor=quantity)
+		for baseIngredientName, baseIngredientQuantity in baseIngredients.items():
+			baseIngredientsReturnDict[baseIngredientName] = baseIngredientsReturnDict.get(baseIngredientName, 0) + baseIngredientQuantity
+	elif itemToExplore["type"] == "ingredient":
+		baseIngredientsReturnDict[itemToExplore["name"]] = baseIngredientsReturnDict.get(itemToExplore["name"], 0) + quantity
+	
+	return baseIngredientsReturnDict
+
+# This function takes in a list of base ingredients 
+# (their names and quantity) and returns the total cooking time
+def cookTimeHelper(baseIngredientsList):
+	
+	# Create a dict of the ingredients and their cooking time for quick lookup
+	cookTimeDict = {ingredient.name: ingredient.cookTime for ingredient in cookbook if hasattr(ingredient, 'cookTime')}
+
+	# Return the sum of all the cooking times in the baseIngredientsDict
+	return sum(cookTimeDict.get(ingredient) * quantity for ingredient, quantity in baseIngredientsList.items() if cookTimeDict.get(ingredient) is not None)
 
 
 # =============================================================================
